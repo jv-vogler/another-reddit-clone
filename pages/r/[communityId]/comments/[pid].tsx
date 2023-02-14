28d4046f79f20081a4d999a4541165e8e5884397
jv-vogler <jvsvogler@gmail.com>
2023-02-14 17:53:24 -0300

    Add commenting creation/deletion functionality

--- pages/r/[communityId]/comments/[pid].tsx
+++ pages/r/[communityId]/comments/[pid].tsx
@@ -1,21 +1,23 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useRouter } from 'next/router';
+import { auth, firestore } from '@/firebase/clientApp';
+import { doc, getDoc } from 'firebase/firestore';
+import { Post } from '@/atoms/postsAtom';
+import { User } from 'firebase/auth';
+import About from '@/components/Community/About';
+import useCommunityData from '@/hooks/useCommunityData';
+import Comments from '@/components/Posts/Comments/Comments';
 import PageContent from '@/components/Layout/PageContent';
 import PostItem from '@/components/Posts/PostItem';
 import usePosts from '@/hooks/usePosts';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth, firestore } from '@/firebase/clientApp';
-import { useRouter } from 'next/router';
-import { doc, getDoc } from 'firebase/firestore';
-import { Post } from '@/atoms/postsAtom';
-import About from '@/components/Community/About';
-import useCommunityData from '@/hooks/useCommunityData';
 
 const PostPage = () => {
   const [user] = useAuthState(auth);
   const { postStateValue, setPostStateValue, onDeletePost, onVote } = usePosts();
-  const router = useRouter();
   const { communityStateValue } = useCommunityData();
+  const router = useRouter();
 
   const fetchPost = async (postId: string) => {
     try {
@@ -53,7 +55,11 @@ const PostPage = () => {
             }
           />
         )}
-        {/* Comments */}
+        <Comments
+          user={user as User}
+          communityId={communityStateValue.currentCommunity?.id as string}
+          selectedPost={postStateValue.selectedPost}
+        />
       </>
       <>
         {communityStateValue.currentCommunity && (
